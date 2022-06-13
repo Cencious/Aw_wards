f63f335d118ab79d92d0ed09f771c012f4904eed
Cencious <user.email>
2022-06-14 02:56:39 +0300

    add app urs  for various functions

--- awards/urls.py
+++ awards/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .views import ListProfileView, ListProjectView
 
 urlpatterns =[
-    path('home/', views.home, name='home'),
-    # path('signup/', views.register, name='signup'),
+    path('', views.home, name='home'),
+    path('project/<project_id>/',views.project,name ='project'),
+    path('search/', views.search_results, name='search_results'),
+    path('profile/', views.profile, name='profile'),
+    path('update/', views.update, name='update'),
+    path('upload_project/', views.upload_project, name='upload_project'),
+    path('rate/<project_id>/',views.rate,name ='rate'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    # path('api/profile/', ListProfileView.as_view(), name="profile-all"),
+    path('api/project/', ListProjectView.as_view(), name="project-all"),
 
 ]
