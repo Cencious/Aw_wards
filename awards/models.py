258a148ce1330c7881e47ef5f9aae3f46647e7af
Cencious <user.email>
2022-06-13 15:38:14 +0300

    define method for rating the projects

--- awards/models.py
+++ awards/models.py
@@ -29,4 +29,20 @@ class Project(models.Model):
     class Meta:
         ordering =['pub_date']
     
-   
+    def __str__(self):
+        return self.title
+    
+    def no_of_ratings(self):
+        ratings = Rating.objects.all(project=self)
+        return len(ratings)
+    
+
+    def average_ratings(self):
+        sum = 0
+        ratings = Rating.objects.filter(project=self)
+        for rating in ratings:
+            sum += ((rating.rate_design + rating.rate_usability + rating.rate_content)/3)
+        if len(ratings) > 0:
+            return sum/len(ratings)
+        else:
+            return 0
