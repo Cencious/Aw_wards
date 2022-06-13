5533faee65c66a997ace169e86bb6e41be77da9c
Cencious <user.email>
2022-06-13 13:40:08 +0300

    Add class Testprofile to test profile variables

--- awards/tests.py
+++ awards/tests.py
@@ -3,3 +3,11 @@ from .models import Profile
 from django.contrib.auth import User
 
 # Create your tests here.
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='kakan')
+
+    def tearDown(self):
+        self.user.delete()
+        
