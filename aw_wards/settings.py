5b5b701e80171d24997542ecb3967939e34131ae
Cencious <user.email>
2022-06-13 17:02:56 +0300

    add postgresql engine

--- aw_wards/settings.py
+++ aw_wards/settings.py
@@ -83,7 +83,11 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'kakan',
+        'PASSWORD':'Abiathar2022'
+
     }
 }
 
