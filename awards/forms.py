594c515debd43f585a41dc69f9334d166812a59b
Cencious <user.email>
2022-06-14 02:58:56 +0300

    ass form for user update and profile update

--- awards/forms.py
+++ awards/forms.py
@@ -10,3 +10,15 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_pic', 'bio']
+
