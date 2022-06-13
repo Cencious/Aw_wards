a5062c1a718d5f7d19aff890f329524531925b51
Cencious <user.email>
2022-06-13 17:14:38 +0300

    add project views

--- awards/views.py
+++ awards/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
-from awards.models import Project, User
+from awards.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.http.response import Http404
 
 
 
@@ -7,8 +9,16 @@ def home(request):
     '''
     View function that renders home
     '''
-    projects = Project.objects.all()
+    # projects = Project.objects.all()
     users = User.objects.all()
 
     return render(request, 'home.html')
 
+def project(request, project_id):
+    try:
+        project = Project.objects.get(id=project_id)
+    
+    except ObjectDoesNotExist:
+        raise Http404()
+
+    return render(request, 'project.html', {'project': project})
