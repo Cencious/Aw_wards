eeba96036084d27d4497736452b8b22e75e451ca
Cencious <user.email>
2022-06-14 03:07:50 +0300

    import views from the app , and create a route to the register

--- aw_wards/urls.py
+++ aw_wards/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from awards import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('awards.urls')),
+    path('register/', user_views.register, name = 'register'),
 ]
