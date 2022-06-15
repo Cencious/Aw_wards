35c29a1b600d53636bb0d5aa68e065bbc943d08c
Cencious <user.email>
2022-06-15 13:44:54 +0300

    update tests

--- awards/tests.py
+++ awards/tests.py
@@ -1,18 +1,37 @@
 from django.test import TestCase
-from .models import Profile
+from .models import Profile,Project,Rating
 from django.contrib.auth.models import User
 
 # Create your tests here.
 
 class TestProfile(TestCase):
     def setUp(self):
-        self.user = User.objects.create(username='kakan')
+        self.test_user = User.objects.create(username='kakan')
 
     def tearDown(self):
-        self.user.delete()
+        self.test_user.delete()
         
     def setUp(self):
-        self.profile = Profile(user='kakan', bio='Never say never', profile_pic='cloudlink.cloud')
+        self.test_profile = Profile(user='kakan', bio='Never say never', profile_pic='cloudlink.cloud')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile, Profile))
+
+
+class RatingTestClass(TestCase):
+    def setUp(self):
+        self.test_rating = Rating(user='kakan', project='News api', review='out_standing', rate_design=10, rate_usability=9, rate_content=8)
+
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_rating, Rating))
+
+    def test_save(self):
+        self.test_rating.save_comment()
+        self.assertEqual(len(Rating.objects.all()), 1)
+    
+
+    def tearDown(self):
+        self.test_user.delete()
+        self.test_rating.delete()
+        Rating.objects.all().delete()
